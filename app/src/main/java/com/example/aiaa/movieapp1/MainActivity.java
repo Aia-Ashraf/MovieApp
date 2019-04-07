4ed2257bbbb827b9d6aabe5f7295256b48522aa1
Aia Ashraf <aia.ashraf94@gmail.com>
2019-04-07 17:46:16 +0200

    add AppWidget + Firebase JobDispatcher

--- app/src/main/java/com/example/aiaa/movieapp1/MainActivity.java
+++ app/src/main/java/com/example/aiaa/movieapp1/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.aiaa.movieapp1;
 
 
+import android.appwidget.AppWidgetManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
+
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.firebase.jobdispatcher.Lifetime;
+import com.firebase.jobdispatcher.RetryStrategy;
+import com.firebase.jobdispatcher.Trigger;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private MovieAdapter movieAdapter;
+    private Context context;
 
 
     public FavouritDatabase mDB;
@@ -39,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        scheduleJob(this);
 
 
         recyclerView = findViewById(R.id.recyclerview);
@@ -153,6 +167,60 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+    public static void scheduleJob(Context context) {
+        //creating new firebase job dispatcher
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(context));
+        //creating new job and adding it with dispatcher
+        Job job = createJob(dispatcher);
+        dispatcher.mustSchedule(job);
+    }
 
+    public static Job createJob(FirebaseJobDispatcher dispatcher){
+
+        Job job = dispatcher.newJobBuilder()
+                //persist the task across boots
+                .setLifetime(Lifetime.FOREVER)
+                //.setLifetime(Lifetime.UNTIL_NEXT_BOOT)
+                //call this service when the criteria are met.
+                .setService(ScheduledJobService.class)
+                //unique id of the task
+                .setTag("UniqueTagForYourJob")
+                //don't overwrite an existing job with the same tag
+                .setReplaceCurrent(false)
+                // We are mentioning that the job is periodic.
+                .setRecurring(true)
+                // Run between 30 - 60 seconds from now.
+                .setTrigger(Trigger.executionWindow(30, 60))
+                // retry with exponential backoff
+                .setRetryStrategy(RetryStrategy.DEFAULT_LINEAR)
+                //.setRetryStrategy(RetryStrategy.DEFAULT_EXPONENTIAL)
+                //Run this job only when the network is available.
+                .setConstraints(Constraint.ON_ANY_NETWORK, Constraint.DEVICE_CHARGING)
+                .build();
+        return job;
+    }
+
+    public static Job updateJob(FirebaseJobDispatcher dispatcher) {
+        Job newJob = dispatcher.newJobBuilder()
+                //update if any task with the given tag exists.
+                .setReplaceCurrent(true)
+                //Integrate the job you want to start.
+                .setService(ScheduledJobService.class)
+                .setTag("UniqueTagForYourJob")
+                // Run between 30 - 60 seconds from now.
+                .setTrigger(Trigger.executionWindow(30, 60))
+                .build();
+        return newJob;
+    }
+
+    public void cancelJob(Context context){
+
+        FirebaseJobDispatcher dispatcher = new FirebaseJobDispatcher(new GooglePlayDriver(context));
+        //Cancel all the jobs for this package
+        dispatcher.cancelAll();
+        // Cancel the job for this tag
+        dispatcher.cancel("UniqueTagForYourJob");
+
+    }
 
 }
---
+++ app/src/main/java/com/example/aiaa/movieapp1/ScheduledJobService.java
@@ -0,0 +1,47 @@
+package com.example.aiaa.movieapp1;
+
+import android.util.Log;
+
+import com.firebase.jobdispatcher.JobParameters;
+import com.firebase.jobdispatcher.JobService;
+
+public class ScheduledJobService extends JobService {
+
+        private static final String TAG = ScheduledJobService.class.getSimpleName();
+
+        @Override
+        public boolean onStartJob(final JobParameters params) {
+            //Offloading work to a new thread.
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    codeYouWantToRun(params);
+                }
+            }).start();
+
+            return true;
+        }
+
+        @Override
+        public boolean onStopJob(JobParameters params) {
+            return false;
+        }
+
+        public void codeYouWantToRun(final JobParameters parameters) {
+            try {
+
+                Log.d(TAG, "completeJob: " + "jobStarted");
+                //This task takes 2 seconds to complete.
+                Thread.sleep(2000);
+
+                Log.d(TAG, "completeJob: " + "jobFinished");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                //Tell the framework that the job has completed and doesnot needs to be reschedule
+                jobFinished(parameters, true);
+            }
+        }
+    }
+
+
