1793b602c52b108b2c599af893fc59e7ad237169
DELL <aia.ashraf94@gmail.com>
2019-05-20 18:11:25 +0200

    add login

--- app/src/main/java/com/example/aiaa/movieapp1/registeration/ResetPasswordActivity.java
+++ app/src/main/java/com/example/aiaa/movieapp1/registeration/ResetPasswordActivity.java
@@ -1,8 +1,6 @@
 package com.example.aiaa.movieapp1.registeration;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +8,10 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.aiaa.movieapp1.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
--- app/src/main/java/com/example/aiaa/movieapp1/registeration/activity_signup.java
+++ app/src/main/java/com/example/aiaa/movieapp1/registeration/activity_signup.java
@@ -9,8 +9,10 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.aiaa.movieapp1.MainActivity;
 import com.example.aiaa.movieapp1.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
